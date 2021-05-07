59fdb3f231722d1f1926da215781ac78c5f30764
sushant12 <sussyoung9@gmail.com>
2021-05-07 10:34:50 +0545

    using dynamic supervisor

--- lib/tahmeel/worker_server.ex
+++ lib/tahmeel/worker_server.ex
@@ -5,8 +5,11 @@ defmodule Tahmeel.WorkerServer do
 
   alias Tahmeel.Cron.Expression
 
-  def start_link(args) do
-    GenServer.start_link(@name, args, name: @name)
+  def start_link({_expr, worker} = args) do
+    # IO.inspect("=========")
+    # IO.inspect(args)
+    # IO.inspect("=========")
+    GenServer.start_link(@name, args, name: worker)
   end
 
   @impl true
@@ -14,7 +17,7 @@ defmodule Tahmeel.WorkerServer do
     {:ok, args, {:continue, :start}}
   end
 
-  @impl GenServer
+  @impl true
   def handle_continue(:start, state) do
     handle_info(:work, state)
   end
@@ -26,11 +29,12 @@ defmodule Tahmeel.WorkerServer do
     {:noreply, state}
   end
 
-  defp maybe_run_jobs(state) do
-    Enum.each(state, fn {expression, worker} ->
-      expr = Expression.parse!(expression)
-      Expression.now?(expr) and Task.start(fn -> apply(worker, :run, []) end)
-    end)
+  defp maybe_run_jobs({_expr, worker}) do
+    Task.start(fn -> apply(worker, :run, []) end)
+    # Enum.each(state, fn {expression, worker} ->
+    #   expr = Expression.parse!(expression)
+    #   Expression.now?(expr) and Task.start(fn -> apply(worker, :run, []) end)
+    # end)
   end
 
   defp schedule_work do
--- lib/tahmeel/worker_supervisor.ex
+++ lib/tahmeel/worker_supervisor.ex
@@ -1,18 +1,41 @@
 defmodule Tahmeel.WorkerSupervisor do
-  use Supervisor
+  use DynamicSupervisor
 
   @name __MODULE__
 
-  def start_link(_args) do
-    Supervisor.start_link(@name, nil, name: @name)
+  alias Tahmeel.WorkerServer
+
+  # def start_link(_args) do
+  #   Supervisor.start_link(@name, nil, name: @name)
+  # end
+
+  # @impl true
+  # def init(nil) do
+  #   children = [
+  #     {Tahmeel.WorkerServer, Application.get_env(:tahmeel, :periodic_jobs)}
+  #   ]
+
+  #   Supervisor.init(children, strategy: :one_for_one)
+  # end
+
+  def start_link(_arg) do
+    DynamicSupervisor.start_link(@name, nil, name: @name)
   end
 
   @impl true
   def init(nil) do
-    children = [
-      {Tahmeel.WorkerServer, Application.get_env(:tahmeel, :periodic_jobs)}
-    ]
+    # jobs = Application.get_env(:tahmeel, :periodic_jobs)
+    # Enum.each(jobs, &start_worker/1)
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
 
-    Supervisor.init(children, strategy: :one_for_one)
+  def start_worker(worker) do
+    child_spec = %{
+      id: WorkerServer,
+      start: {WorkerServer, :start_link, [worker]},
+      restart: :transient
+    }
+
+    DynamicSupervisor.start_child(@name, child_spec)
   end
 end
